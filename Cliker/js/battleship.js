c9b3203ddc960ae6943505a98022efeb3cf7dbf4
mamontXX <49881354+mamontXX@users.noreply.github.com>
2019-11-29 16:32:03 +0300

    Clicker

---
+++ Cliker/js/battleship.js
@@ -0,0 +1,12 @@
+'use strict'
+window.onload = function () {
+  var count = 0;
+  var message = "You clicked me ";
+  var div = document.getElementById("message")
+
+  var button = document.getElementById("clickme");
+  button.onclick = function () {
+    count++;
+    div.innerHTML = message + count + "times!";
+  };
+};
